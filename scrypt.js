49dec23674fa50f779f2bf30982968c0bdbfff33
ricardoairesm <ricardoaamb@usp.br>
2022-10-31 16:17:46 -0300

    primeira versao funcional

--- scrypt.js
+++ scrypt.js
@@ -1,11 +1,15 @@
+let principal = document.getElementById("principal");
+let passar = {};
 manterConectado();
 start();
 
-const mensagem = { from: "", to: "", text: "", type: "", time: "" };
-const principal = document.getElementById("principal");
+
+
 
 
 function start() {
+
+    principal.innerHTML = "";
 
     let mensagens = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
     mensagens.then(printar);
@@ -14,42 +18,62 @@ function start() {
 
         for (let i = 0; i < promessa.data.length; i++) {
             if (promessa.data[i].type == "message") {
-                principal.innerHTML += `<div class="mensagem-normal">
-        <p><span>  (${promessa.data[i].time})  <h1>  ${promessa.data[i].from}  </h1></span>${promessa.data[i].text}</p>
-    </div>`
+                principal.innerHTML += `<div class="mensagem-normal mensagem">
+              
+                <div class = "time">  (${promessa.data[i].time})  </div> <p> <span>${promessa.data[i].from}</span> para <span>${promessa.data[i].to}</span>${promessa.data[i].text}<p>
+                 </div>`
+                
             }
             if (promessa.data[i].type == "status") {
-                principal.innerHTML += `<div class="mensagem-status">
-        <p><span>  (${promessa.data[i].time})  <h1>  ${promessa.data[i].from}  </h1></span>${promessa.data[i].text}</p>
-    </div>`
+                principal.innerHTML += `<div class="mensagem-status mensagem">
+                <div class = "time">  (${promessa.data[i].time})  </div> <p> <span>${promessa.data[i].from}</span> ${promessa.data[i].text}</p>
+                </div>`
+                
             }
             if (promessa.data[i].type == "private_message") {
-                principal.innerHTML += `<div class="mensagem-reservada">
-        <p><span>  (${promessa.data[i].time})  <h1>  ${promessa.data[i].from}  </h1></span> reservadamente para <h1>  ${promessa.data[i].to}  </h1>${promessa.data[i].text}</p>
-    </div>`
+                principal.innerHTML += `<div class="mensagem-reservada mensagem">
+                <div class = "time">  (${promessa.data[i].time})  </div> <p> <span>${promessa.data[i].from}</span> reservadamente para <span>${promessa.data[i].to}</span>${promessa.data[i].text}<p>
+                </div>`
+                
             }
         }
+
+
+
         setTimeout(() => {
             start();
         }, 3000);
+
+        Refresh();
+
+
     }
+   
+    return principal;
+   
+
+
 }
 
 function manterConectado() {
-    let passar = {
-         name: prompt("Qual o seu nome?")
-     }
-     axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", passar)
-     .then(tratarSucesso)
-     .catch(tratarErro);
- 
- 
+    passar = {
+        name: prompt("Qual o seu nome?")
+    }
+    axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", passar)
+        .then(tratarSucesso)
+        .catch(tratarErro);
+
+
     function tratarSucesso() {
+        check();
+        start();
+    }
+
+    function check() {
         axios.post("https://mock-api.driven.com.br/api/v6/uol/status", passar);
         setTimeout(() => {
-            tratarSucesso();
+            check();
         }, 5000);
-        console.log("sucesso!");
     }
 
     function tratarErro(erro) {
@@ -63,9 +87,29 @@ function manterConectado() {
             tratarSucesso();
         }
     }
-
-
+    return passar;
 }
+
+function Refresh() {
+    let pointer = document.getElementById("principal");
+    pointer.lastChild.scrollIntoView();
+}
+
+function mandarMensagem() {
+    console.log(passar.name);
+    let texto = document.getElementById("texto");
+    texto = texto.value;
+    let objeto = {
+        from: passar.name,
+        to: "Todos",
+        text: texto,
+        type: "message"
+    }
+    axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", objeto)
+        .then(Refresh());
+}
+
+
 
 
 
